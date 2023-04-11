e5a263ecf3adaad7003beabd050671f1116ca03b
Calmak <cmakangadza@outlook.com>
2023-04-10 17:23:51 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: Always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
