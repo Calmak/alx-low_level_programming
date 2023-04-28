2348734ff3352e0fa12815831bdd602ca29c981a
Calmak <cmakangadza@outlook.com>
2023-04-27 17:38:42 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,7 +8,7 @@
  * to the operator given as a parameter.
  */
 
-int (get_op_func(char *s))(int, int)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
@@ -21,7 +21,7 @@ int (get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op != *s)
+	while (ops[i].op != NULL && * (ops[i].op != *s))
 			i++;
 
 			return (ops[i].f);
