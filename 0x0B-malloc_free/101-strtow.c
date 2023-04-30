468f1ee365d581cfc1f878ade31a52025f697839
Calmak <cmakangadza@outlook.com>
2023-04-30 09:50:12 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,29 +3,28 @@
 /**
  * count_word - count the number of words in a string
  * @s: string
- * Return number of words
+ * Return: number of words
  */
 
-int count_word(char * s)
+int count_word(char *s)
 {
-	int flag, c, w;
+	int a, b, c;
 
-	flag = 0;
-	w = 0;
+	a = 0;
+	c = 0;
 
-	for (c = 0; s[c] != '\0'; c++)
+	for (b = 0; s[b] != '\0'; b++)
 	{
-		if (s[c] =='')
-			flag = 0;
-		
-		else if (flag == 0)
+		if (s[b] == ' ')
+			a = 0;
+		else if (a == 0)
 		{
-			flag = 1;
+			a = 1;
 
-			w++;
+			c++;
 		}
 	}
-	return (w);
+	return (c);
 }
 
 /**
@@ -38,47 +37,42 @@ int count_word(char * s)
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
+	int i, j, len, words, c, start, end;
+
+	j = 0;
+	len = 0;
+	c = 0;
 
 	while (*(str + len))
-		len ++;
-	words = count_words(str);
-
+		len++;
+	words = count_word(str);
 	if (words == 0)
 		return (NULL);
-
-	matrix = (char **) malloc(sizeof(char*) * (words + 1));
-
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-
 	for (i = 0; i <= len; i++)
 	{
-		if (str[i] == '' || str[i] == '\0')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
 			if (c)
 			{
 				end = i;
-
-				tmp = (char *) malloc(sizeof*(char) * (c + 1));
-
+				tmp = (char *) malloc(sizeof(char)*(c + 1));
 				if (tmp == NULL)
-					return(NULL);
-
+					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
-
-				matrix[k] = tmp - c;
-
-				k++;
+				matrix[j] = tmp - c;
+				j++;
 				c = 0;
 			}
 		}
-		else if (c++ = 0)
+		else if (c++ == 0)
 			start = i;
 	}
-	matrix[k] = NULL;
+	matrix[j] = NULL;
 
 	return (matrix);
 }
