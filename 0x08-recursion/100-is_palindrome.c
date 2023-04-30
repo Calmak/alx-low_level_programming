11bc7449827e22b877f35b81593ad3ca74b7ffe1
Calmak <cmakangadza@outlook.com>
2023-04-30 07:16:21 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,53 +1,48 @@
 #include "main.h"
-/**
- * _strlen1 - checks the lenght of a string
- * @s: the string
- * Return: returns the length of the string
- */
-int _strlen1(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen1(s + 1));
-}
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- * _is_palindrome1 - checks if string is a palindrome
- * @i: is the index
- * @l: length of string
- * @s: the string
- * Return: 1 if palindrome o if not
- */
-
-int _is_palindrome1(int i, int l, char *s)
-{
-	if (l > 0)
-	{
-		if (s[i] == s[l])
-		{
-			return (_is_palindrome1(i + 1, l + 1, s));
-		}
-		else if (s[i] != s[l])
-		{
-			return (0);
-		}
-		else
-		{
-			return (1);
-		}
-	}
-	return (1);
-}
-
-/**
- * is_palindrome - returns 1 if a string as a palindrome
- * and 0 if not
- * @s: the strinG
- * Return: 1 if string is palindrome 0 if not
- *
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ * Return: 1 (success), 0 Fail
  */
 
 int is_palindrome(char *s)
 {
-	return (_is_palindrome1(0, _strlen1(s) - 1, s));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calcutate length of
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iteration
+ * @len:
+ * length of string
+ * Return: 1 (success) 0 (fail)
+ */
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
